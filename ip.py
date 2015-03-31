c4c02373e6d55c13b4a35c85835d05d5306d1832
ghking <ghking13@gmail.com>
2015-03-31 00:43:13 -0400

    ip changes

--- ip.py
+++ ip.py
@@ -1,109 +1,55 @@
 import sys, os
 import struct
 
-#Need to keep track of state for the Identification field and fragment offset
+# Questions
+# Doesn't the kernel handle IP checksums?
 
-'''
-Header is of the form
- 0                   1                   2                   3  
- 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|Version|  IHL  |Type of Service|          Total Length         |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|         Identification        |Flags|      Fragment Offset    |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|  Time to Live |    Protocol   |         Header Checksum       |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                       Source Address                          |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                    Destination Address                        |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                    Options                    |    Padding    |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-'''
+class IP(object):
 
-class IPHeader:
+    def sendData(data):
+        # Send it
+        # TODO: Is the remote IP correct? 
+        # TODO: Is the checksum correct? 
+        # TODO: Does the protocol identifier match the contents of the encapsulated header?
+        # TODO: Handle timeout
 
-    def __init__(self, source_address, dest_address):
+class IPPacket(object):
 
+    def __init__(self, source, destination, data):
+
+        self.version = 4
         self.ihl = 5
-        self.type_of_service = 0
-        self.total_length = 0
-        self.id = 0
-        self.fragmentation_offset = 0
+        self.tos = 0
+        self.total_length = 0 # Will be filled by kernel
+        self.id = 54321
+        self.flags = 0
+        self.offset = 0
         self.ttl = 255
         self.protocol = socket.IPPROTO_TCP
-        self.checksum = 
-        self.source_address = source_address
-        self.dest_address = dest_address
+        self.checksum = 0 # Will be filled by kernel
+        self.source = socket.inet_aton(source)
+        self.destination = socket.inet_aton(destination)
+        self.data = data
 
-    def __init__(self, data):
+    def toData(self):
 
-        unpacked = unpack('!BBHHHBBH4s4s' , self.ihl, self.type_of_service, self.total_length, self.id, self.fragmentation_offset, self.ttl, self.protocol, self.checksum, self.source_address, self.dest_address)
-        self.ihl = unpacked[0]
-        self.type_of_service = unpacked[1]
-        self.total_length = unpacked[2]
-        self.id = unpacked[3]
-        self.fragmentation_offset = unpacked[4]
-        self.ttl = unpacked[5]
-        self.protocol = unpacked[6]
-        self.checksum = unpacked[7]
-        self.source_address = unpacked[8]
-        self.dest_address = unpacked[9]
+        version_ihl = (self.version << 4) + self.ihl
+        flags_offset = (self.flags << 13) + self.offset
 
-    def to_data():
-        return pack('!BBHHHBBH4s4s' , self.ihl, self.type_of_service, self.total_length, self.id, self.fragmentation_offset, self.ttl, self.protocol, self.checksum, self.source_address, self.dest_address)
+        ip_header = struct.pack("!BBHHHBBH4s4s",
+                    version_ihl,
+                    self.tos,
+                    self.tl,
+                    self.id,
+                    flags_offset,
+                    self.ttl,
+                    self.protocol,
+                    self.checksum,
+                    self.source,
+                    self.destination)
 
-def calcIpChecksum(header):
-    checksum = 0
-    
-    shorts = struct.unpack('HHHHHHHHHH', header)
+        return ip_header + self.data
 
-    for short in shorts:
-	checksum += short
-	if checksum & 0x10000:
-	    checksum &= 0xFFFF
-	    checksum += 1
-
-    return (~checksum) & 0xFFFF
-
-#Header always 20 Bytes as no options are used
-#Assuming len(data) <= 1480
-def makeIpHeader(source_ip, dest_ip, data):
-    Version = 0b0100 #ipv4
-    IHL = 0b0101 #size of header in 4byte words
-    Type = 0x00
-    TotalLen = len(data) + 20
-    ID = 0
-    Flags = 0b000
-    FragmentOffset = 0
-    TTL = 0x40
-    Protocol = 0x06
-    SourceAddr = source_ip
-    DestAddr = dest_ip    
-
-    #fmt = (network order) byte, byte, short
-    header1 = struct.pack("!BBH", (Version << 4) + IHL, Type, TotalLen) 
-    header1 += struct.pack("!HH", ID, Flags << 13 + FragmentOffset)
-    header1 += struct.pack("!BB", TTL, Protocol)
-    header2 = struct.pack("!LL", SourceAddr, DestAddr)
-
-    Checksum = struct.pack("!H", calcIpChecksum(header1 + '\x00\x00' +  header2))
-
-    header = header1 + Checksum + header2
-
-    return header
-
-def makeIpPacket(source_ip, dest_ip, data):
-    header = makeIpHeader(source_ip, dest_ip, data)
-    packet = header + data
-
-    return packet
-
-
-def extractIpHeader(data):
-    return data[:20]
-
-def extractIpData(data):
-    return data[20:]
-
+    @classmethod
+    def getData(packet_data):
+        return data[20:]
